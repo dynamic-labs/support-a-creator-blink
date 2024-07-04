a126e5eb2bf1349f5f5d4172341bce027391f456
Matthew Foyle <mfoyle93@gmail.com>
2024-07-04 12:37:02 +0200

    fix: second attempt at cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
     async headers() {
-        return [
+      return [
+        {
+          source: '/(.*)',
+          headers: [
             {
-                // matching all API routes
-                source: '/(.*)',
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://www.dial.to"},
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
-};
-
-export default nextConfig;
+              key: 'Access-Control-Allow-Credentials',
+              value: 'true',
+            },
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: 'https://www.dial.to',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
